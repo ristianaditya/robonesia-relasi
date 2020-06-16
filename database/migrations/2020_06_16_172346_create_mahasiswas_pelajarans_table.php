7c5d35b1939443b4e1524b61c436ea9a36f11ab5
disastra02 <dikadisastra@gmail.com>
2020-06-17 00:52:07 +0700

    feat: laravel ui & auth

---
+++ database/migrations/2020_06_16_172346_create_mahasiswas_pelajarans_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMahasiswasPelajaransTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mahasiswas_pelajarans', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('mahasiswa_id')->constrained()->unique();
+            $table->foreignId('pelajaran_id')->constrained()->unique();
+            $table->string('nilai');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mahasiswas_pelajarans');
+    }
+}
